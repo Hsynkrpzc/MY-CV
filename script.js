9c65e8b0c79c8ceae9362dc7b119e4bac3fc4354
hsynkrpz <hsynkrpzc@gmail.com>
2022-12-26 22:06:36 +0300

    v3

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ pdfBtn.addEventListener("click", function () {
   const myCv = document.getElementById("myCv");
   const nameSurname = document.getElementById("nameSurname");
   var opt = {
-    margin: 1,
+    margin: 0,
     filename: nameSurname.innerText + ".pdf",
     image: { type: "jpeg", quality: 0.98 },
     html2canvas: { scale: 2 },
